d070ccab7e3bf4633af43406f4f8409e04e68421
Dillon Kissoon <dillonkissoon@users.noreply.github.com>
2023-09-22 16:58:22 +0300

    fix workflow syntax error

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/marketing.app";
 export default () => {
   return (
     <div>
-      <h1>Hi from Container!!!</h1>
+      <h1>Hi from Container!!</h1>
       <hr />
       <MarketingApp />
     </div>
